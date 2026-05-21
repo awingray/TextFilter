4d6250ecff4ef91d3f2ed74306fa1660b6760229
Awin Gray <grayawin@gmail.com>
2026-05-21 15:55:50 +0700

    initial exploration

--- src/TextFilter/Program.cs
+++ src/TextFilter/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+const string testString = "hello:  , how are   you doing?\n THIS IS A TEST/ string (). Lorem?'IPSUM";
+
+string GetWordFromText(string text)
+{
+    var word = new StringBuilder();
+    foreach (var  c in text)
+    {
+        if (!char.IsLetter(c)) break;
+        word.Append(c);
+    }
+    return word.ToString();
+}
+
+string[] TextIterator(string text) => testString.Split(' ', '?', ',', '\'', ';'); // This is hideous but it will do for now
+
+foreach (var c in TextIterator(testString))
+{
+    Console.WriteLine(GetWordFromText(c));
+}
+
+
+
