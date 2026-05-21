fbeab55251a8a086da1d7c2e5783ded73dbf9087
Awin Gray <grayawin@gmail.com>
2026-05-21 16:57:51 +0700

    fix build errors, and output

--- src/TextFilter/Program.cs
+++ src/TextFilter/Program.cs
@@ -1,10 +1,11 @@
-﻿using System.Text;
-using TextFilter;
+﻿using TextFilter;
 using TextFilter.Filters;
 
 const string testString = "hello:  , how are   you doing?\n THIS IS A TEST/ string (). Lorem?'IPSUM";
 
 var wordSpec = new WordSpec().AddSpec(new NoLetterT());
-var textFilter = new TextFilter.TextFilter(new Tokenizer(), wordSpec);`
+var textFilter = new TextFilter.TextFilter(new Tokenizer(), wordSpec);
+
+Console.WriteLine(textFilter.Filter(testString));
 
 
--- src/TextFilter/Tokenizer.cs
+++ src/TextFilter/Tokenizer.cs
@@ -10,5 +10,5 @@ internal sealed class Tokenizer : ITokenizer
 {
     // This is hideous and doesn't scale but it will do for now
     // I just realized one hour in that this actually doesn't work...
-    public string[] Tokenize(string testString) => testString.Split(' ', '?', ',', '\'', ';', '\n'); 
+    public string[] Tokenize(string testString) => testString.Split(' ', '?', ',', '\'', ';', '\n', '(', ')', '.'); 
 }
